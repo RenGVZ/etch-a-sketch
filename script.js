432d1854e50b09a57d3face476d1a7deb44202e9
gavingarza <gaving@live.com>
2020-06-01 23:57:45 +0900

    created a method to generate grid on click event

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('yo what up');
+const grid = document.querySelector('#grid');
+const btn  = document.querySelector('#btn');
+
+btn.addEventListener('click', createGrid)
+
+function createGrid() {
+  for(x = 0; x <= 16; x++){
+  let div = document.createElement('div');
+  
+  
+  }
+  // document.body.appendChild('div');
+}
