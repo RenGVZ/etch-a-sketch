a550fe020a0a89999576587f00cb12e290772453
gavingarza <gaving@live.com>
2020-06-02 23:17:09 +0900

    added functionality to grid creation and changed css including image background

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const grid = document.querySelector('#grid');
-const btn  = document.querySelector('#btn');
+let numberOfDivs;
+let grid = document.querySelector('#grid');
+let gridItem = [];
+const go = document.querySelector('#go');
 
-btn.addEventListener('click', createGrid)
+go.addEventListener('click', createSketchPad)
 
-function createGrid() {
-  for(x = 0; x <= 16; x++){
-  let div = document.createElement('div');
-  
-  
+function createSketchPad() {
+  let numberOfColumns = 16;
+  grid.style.cssText = `grid-template-columns: repeat(${numberOfColumns}, 1fr)`;
+  numberOfDivs = numberOfColumns * numberOfColumns;
+
+  function createGrid(x) {
+    for(i = 0; i <= x; i++){
+    gridItem[i] = document.createElement('div');
+    gridItem[i].classList = 'grid-item';
+    gridItem[i].setAttribute('id', `item${[i]}`);
+    grid.appendChild(gridItem[i]);
+    }
   }
-  // document.body.appendChild('div');
+  createGrid(numberOfDivs);
+  return numberOfDivs
 }
