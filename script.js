bd522b91a1aad80e53c3ebec82ced67551ea5903
gavingarza <gaving@live.com>
2020-06-03 23:46:50 +0900

    implementing a reSize function to re calculate the grid size

--- script.js
+++ script.js
@@ -3,19 +3,20 @@ let grid = document.querySelector('#grid');
 let gridItem = [];
 const go = document.querySelector('#go');
 const reSize = document.querySelector('#re-size');
-console.log(reSize.value)
+let gridValue = document.querySelector('#input-size').value;
 
 document.addEventListener('DOMContentLoaded', createSketchPad);
 
 function createSketchPad() {
-  let gridValue = document.querySelector('input').value;
   let numberOfColumns = gridValue;
-  // console.log(numberOfColumns);
   grid.style.cssText = `grid-template-columns: repeat(${numberOfColumns}, 1fr)`;
   numberOfDivs = numberOfColumns * numberOfColumns;
 
+  createGrid(numberOfDivs);
+  return numberOfDivs
+}
   function createGrid(x) {
-    x = gridValue;
+    // x = gridValue;
     for(i = 0; i <= x; i++){
     gridItem[i] = document.createElement('div');
     gridItem[i].classList = 'grid-item';
@@ -23,20 +24,36 @@ function createSketchPad() {
     grid.appendChild(gridItem[i]);
     }
   }
-  createGrid(numberOfDivs);
-  return numberOfDivs
-}
+  
 
 reSize.addEventListener('click', reSizeGrid);
+
+function reSizeGrid() {
+  console.log(gridValue)
+  for(i = 0; i < grid.children; i++){
+    grid.removeChild(gridItem[i]);
+  }
+  createGrid(gridValue);
+  // gridItem.length = 0;
+  // let gridValue = document.querySelector('input').value;
+  // let numberOfColumns = gridValue;
+
+
+}
 
 
 grid.addEventListener('mousemove', (event) => {
   let target = event.target;
   target.style.background = 'red';
+  grid.addEventListener('mouseleave', (event) => {
+  event.preventDefault();
 });
-grid.addEventListener('mouseleave', (event) => {
+grid.addEventListener('mouseenter', (event) => {
   event.preventDefault();
 })
+});
+
+
 
 // function colorSquares {
 //   console.log(target.value)
