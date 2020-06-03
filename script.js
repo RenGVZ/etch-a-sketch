dfdc34de1849ab1db403157e722afe5b475c1d59
gavingarza <gaving@live.com>
2020-06-03 23:35:40 +0900

    allows individual divs to become pixelated after mousemove event

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ let numberOfDivs;
 let grid = document.querySelector('#grid');
 let gridItem = [];
 const go = document.querySelector('#go');
+const reSize = document.querySelector('#re-size');
+console.log(reSize.value)
 
-go.addEventListener('click', createSketchPad)
+document.addEventListener('DOMContentLoaded', createSketchPad);
 
 function createSketchPad() {
-  let numberOfColumns = 16;
+  let gridValue = document.querySelector('input').value;
+  let numberOfColumns = gridValue;
+  // console.log(numberOfColumns);
   grid.style.cssText = `grid-template-columns: repeat(${numberOfColumns}, 1fr)`;
   numberOfDivs = numberOfColumns * numberOfColumns;
 
   function createGrid(x) {
+    x = gridValue;
     for(i = 0; i <= x; i++){
     gridItem[i] = document.createElement('div');
     gridItem[i].classList = 'grid-item';
@@ -21,3 +26,18 @@ function createSketchPad() {
   createGrid(numberOfDivs);
   return numberOfDivs
 }
+
+reSize.addEventListener('click', reSizeGrid);
+
+
+grid.addEventListener('mousemove', (event) => {
+  let target = event.target;
+  target.style.background = 'red';
+});
+grid.addEventListener('mouseleave', (event) => {
+  event.preventDefault();
+})
+
+// function colorSquares {
+//   console.log(target.value)
+// }
